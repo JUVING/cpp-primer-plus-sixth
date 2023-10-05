2285160fd3254311f63e91eb83940359b274e34a
JUVING <129962308+JUVING@users.noreply.github.com>
2023-10-06 02:47:25 +0900

    수정

--- c++ 17.cpp
+++ c++ 17.cpp
@@ -714,3 +714,21 @@ int main()
 
 	return 0;
 }*/
+
+
+
+
+/*
+//strin.cpp 17-22 (1450pg)
+int main()
+{
+	string lit = "길섶 민들레 꽃씨대롱 방울로 부풀어"
+				 "여윈 목 아프게 뺴어 들고"
+				 "아가씨 실어 나를 바람 기다리 누나";
+	istringstream instr(lit);
+	string word;
+	while (instr >> word)
+		cout << word << endl;
+
+	return 0;
+}*/
